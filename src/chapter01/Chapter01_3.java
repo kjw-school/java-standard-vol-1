6da5e657a6b24b9f9eb4688f1f1815334b7cfad2
kjw1995 <114547095+kjw1995@users.noreply.github.com>
2024-09-23 23:45:27 +0900

    Study Day 2

---
+++ src/chapter01/Chapter01_3.java
@@ -0,0 +1,12 @@
+package chapter01;
+
+/**
+ * 3. 자바로 프로그램작성하기
+ */
+public class Chapter01_3 {
+
+    /**
+     * 3.1 Hello.java
+     */
+
+}
---
+++ src/chapter02/Chapter02_2.java
@@ -0,0 +1,7 @@
+package chapter02;
+
+/**
+ * 2. 변수의 타입
+ */
+public class Chapter02_2 {
+}
