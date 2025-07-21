98c9361e25f6c8354f0356885d7fc70247367426
kjw1995 <114547095+kjw1995@users.noreply.github.com>
2025-07-22 00:01:45 +0900

    Day42 - Chapter03_4, Chapter03_5

--- src/chapter03/Chapter03_4.java
+++ src/chapter03/Chapter03_4.java
@@ -1,5 +1,7 @@
 package chapter03;
 
+import static java.lang.System.*;
+
 /**
  * <h1>4. 비교 연산자</h1><br>
  * 비교 연산자는 두 피연산자를 비교하는 데 사용되는 연산자다. 주로 조건문과 반복문의 조건식에 사용되며, 연산결과는 오직 true와 false 둘 중의 하나이다.<br>
@@ -71,9 +73,41 @@ public class Chapter03_4 {
 	}
 
 	/**
-	 * <h5>문자열의 비교</h5>
+	 * <h5>문자열의 비교</h5><br>
+	 * 두 문자열을 비교할 때는, 비교 연산자 '=='대신 equals()라는 메서드를 사용해야 한다.<br>
+	 * 비교 연산자는 두 문자열이 완전히 같은 것인지 비교할 뿐이므로, 문자열의 내용이 같은지 비교하기 위해서는 equals()를 사용하는 것이다.<br>
+	 * equals()는 비교하는 두 문자열이 같으면 true를, 다르면 false를 반환한다.<br>
+	 * 원래 String은 클래스이므로, 아래와 같이 new를 사용해서 객체를 생성해야한다.<br>
+	 * <pre><code>
+	 *     String str = new String("abc"); // String클래스의 객체를 생성
+	 *     String str = "abc"; // 위의 문장을 간단히 표현
+	 * </code></pre>
+	 * <br>
 	 */
 	class Memo3 {
+
+	}
+
+	/**
+	 * str2와 "abc"의 내용이 같은데도 "=="로 비교하면, false를 결과로 얻는다.<br>
+	 * 내용은 같지만 서로 다른 객체라서 그렇다. 그러나 equals()는 객체가 달라도 내용이 같으면 true를 반환한다.<br>
+	 * 그래서 문자열을 비교할 때는 항상 equals()를 사용해야 한다는 것을 기억하자.<br>
+	 * 대소문자를 구별하지 않고 비교하고 싶으면, equals()대신 equalsIgnoreCase()를 사용하면 된다.
+	 */
+	class Ex01 {
+
+		public static void main(String[] args) {
+			String str1 = "abc";
+			String str2 = new String("abc");
+
+			out.printf("\"abc\"==\"abc\" ? %b%n", "abc" == "abc");
+			out.printf(" str1==\"abc\"  %b%n", str1 == "abc");
+			out.printf(" str2==\"abc\" ? %b%n", str2 == "abc");
+			out.printf("str1.equals(\"abc\") ? %b%n", str1.equals("abc"));
+			out.printf("str2.equals(\"abc\") ? %b%n", str2.equals("abc"));
+			out.printf("str2.equals(\"ABC\") ? %b%n", str2.equals("ABC"));
+			out.printf("str2.equalsIgnoreCase(\"ABC\") ? %b%n", str2.equalsIgnoreCase("ABC"));
+		}
 
 	}
 
