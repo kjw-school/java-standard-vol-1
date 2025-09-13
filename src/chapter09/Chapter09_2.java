766aff96a04b55f4399fac53b2b99c7c785e116e
kjw1995 <114547095+kjw1995@users.noreply.github.com>
2025-09-14 05:01:01 +0900

    Day52 - Chapter09_2

--- src/chapter09/Chapter09_2.java
+++ src/chapter09/Chapter09_2.java
@@ -126,8 +126,264 @@ public class Chapter09_2 {
 
 	/**
 	 * <h5>2.4 java.util.Scanner클래스</h5><br>
+	 * Scanner는 화면, 파일, 문자열과 같은 입력소스로부터 문자데이터를 읽어오는데 도움을 줄 목저으로 JDK1.5부터 추가되었다.<br>
+	 * Scanner는 정규식 표현(Regular expression)을 이용한 라인단위의 검색을 지원하며 구분자(delimiter)에도 정규식 표현을 사용할 수 있어서 복잡한 형태의 구분자도 처리가 가능하다.<br>
 	 */
 	class Memo5 {
+
+	}
+
+	/**
+	 * <h5>2.5 java.util.StringTokenizer클래스</h5><br>
+	 * StringTokenizer는 긴 문자열을 저자된 구분자(delimiter)를 기준으로 토큰(token)이라는 여러 개의 문자열로 잘라내는 데 사용된다.<br>
+	 * stringTokenizer를 이용하는 방법 이외에도 아래와 같이 String의 split(String regex)이나 Scanner의 userDelimiter(String pattern)를 사용할 수도 있지만,<br>
+	 * 이 두가지 방법은 정규식 표현(Regular expression)을 사용해야하므로 정규식 표현에 익숙하지 않은 경우 StringTokenizer를 사용하는 것이 간단하면서도 명확한 결과를 얻을 수 있을 것이다.
+	 */
+	class Memo6 {
+
+	}
+
+	/**
+	 * <h5>2.6 java.math.BigInteger클래스</h5><br>
+	 * 정수형으로 표현할 수 있는 값의 한계가 있다. 가장 큰 정수형 타입인 long으로 표현할 수 있는 값을 10진수로 19자리 정도이다.<br>
+	 * <pre><code>
+	 *     final int signum; // 부호. 1(양수), 0, -1(음수) 셋 중의 하나
+	 *     final int[] mag; // 값(magnitude)
+	 * </code></pre>
+	 * BigInteger는 내부적으로 int배열을 사용해서 값을 다룬다. 그래서 long타입보다 훨씬 큰 값을 다룰 수 있는 것이다. 대신 성능은 long타입보다 떨어질 수밖에 없다.<br>
+	 * BigInteger는 String처럼 불변(immutable)이다. 모든 정수형이 그렇듯이 BigInteger 역시 값을 '2의 보수'의 형태로 표현한다.
+	 */
+	class Memo7 {
+
+	}
+
+	/**
+	 * <h5>BigInteger의 생성</h5><br>
+	 * BigInteger를 생성하는 방법은 여러 가지가 있는데, 문자열로 숫자를 표현하는 것이 일반적이다. 정수형 리터럴로는 표현할 수 있는 값의 한계가 있기 때문이다.
+	 */
+	class Memo8 {
+
+	}
+
+	/**
+	 * <h5>다른 탕비으로의 변환</h5><br>
+	 * BigInteger를 문자열, 또는 byte배열로 변환하는 메서드<br>
+	 * <pre><code>
+	 *     String toString() // 문자열로 변환
+	 *     String toString(int radix) // 지정된 진법(radix)의 문자열로 변환
+	 *     byte[] toByteArray() // byte배열로 변환
+	 * </code></pre>
+	 * <br>
+	 * BigInteger도 Number로부터 상속받은 기본형으로 변환하는 메서드들을 가지고 있다.<br>
+	 * <pre><code>
+	 *     int intValue()
+	 *     long longValue()
+	 *     float floatValue()
+	 *     double doubleValue()
+	 * </code></pre>
+	 * <br>
+	 * 정수형으로 변환하는 메서드 중에서 이름 끝에 'Exact'가 붙은 것들은 변환한 결과가 변환한 타입의 범위에 속하지 않으면 ArithmeticException을 발생시킨다.<br>
+	 * <pre><code>
+	 *     byte byteValueExact()
+	 *     int intValueExact()
+	 *     long longValueExact()
+	 * </code></pre>
+	 */
+	class Memo9 {
+
+	}
+
+	/**
+	 * BigInteger의 연산<br>
+	 * <pre><code>
+	 * 		BigInteger add(BigInteger val) // 덧셈(this + val)
+	 * 	    BigInteger subtract(BigInteger val) // 뺄셈(this - val)
+	 * 	    BigInteger multiply(BigInteger val) // 곱셈(this * val)
+	 * 	    BigInteger divide(BigInteger val) // 나눗셈(this / val)
+	 * 	    BigInteger remainder(BigInteger val) // 나머지(this % val)
+	 * </code></pre>
+	 * <br>
+	 * BigInteger는 불변이므로, 반환타입이 BigInteger란 얘기는 새로운 인스턴스가 변환된다는 뜻이다.
+	 */
+	class Memo10 {
+
+	}
+
+	/**
+	 * <h5>비트 연산 메서드</h5><br>
+	 * <pre><code>
+	 *     int bitCount() // 2진수로 표현했을 때, 1의 개수(음수는 0의 개수)
+	 *     int bitLength() // 2진수로 표현했을 때, 값을 표현하는데 필요한 bit수
+	 *     boolean testBit(int n) // 우측에서 n+1번째 비트가 1이면 true, 0이면 false
+	 *     BigInteger setBit(int n) // 우측에서 n+1번째 비트를 1로 변경
+	 *     BigInteger clearBit(int n) // 우측에서 n+1번째 비트를 0으로 변경
+	 *     BigInteger flipBit(int n) // 우측에서 n+1번째 비트를 전환(1->0, 0->1)
+	 * </code></pre>
+	 */
+	class Memo11 {
+
+	}
+
+	/**
+	 * <h5>2.7 java.math.BigDecimal클래스</h5><br>
+	 * double타입으로 표현할 수 있는 값을 상당히 범위가 넓지만, 정밀도가 최대 13자리 밖에되지 않고 실수형의 특성상 오차를 피할 수 없다.<br>
+	 * BigDecimal은 실수형과 달리 정수를 이용해서 실수를 표현한다.<br>
+	 * 정수 x 10<sup>-scale</sup><br>
+	 * scale은 0부터 Integer.MAX_VALUE사이의 범위에 있는 값이다. BigDecimal은 정수를 저장하는데 BigInteger를 사용한다.<br>
+	 * <small>※ BigInteger처럼 BigDecimal도 불변(immutable)이다.</small>
+	 * <br>
+	 * <pre><code>
+	 *     private final BigInteger intVal; // 정수(unscaled value)
+	 *     private final int scale; // 지수(scale)
+	 *     private transient int precision; // 정밀도(precision) - 정수의 자릿수
+	 * </code></pre>
+	 * 123.45는 12345 x 10<sup>-2</sup>로 표현할 수 있으며, 이 값이 BigDecimal에 저장되면, intVal의 값은 12345가 되고 scale의 값은 2가 된다.<br>
+	 * scale은 소수점 이하의 자리수를 의미한다는 것을 알 수있다. 그리고 precision의 값은 5가 되는데, 이 값은 정수의 전체 자리수를 의미한다.
+	 */
+	class Memo12 {
+
+	}
+
+	/**
+	 * <h5>BigDecimal의 생성</h5><br>
+	 * 문자열로 숫자를 표현하는 것이 일반적이다. 기본형 리터럴로는 표현할 수 있는 값의 한계가 있기 때문이다.<br>
+	 * 한 가지 주의할 점은, double타입의 값을 매개변수로 갖는 생성자를 사용하면 오차가 발생할 수 있다는 것이다.
+	 */
+	class Memo13 {
+
+	}
+
+	/**
+	 * <h5>다른 타입으로의 변환</h5><br>
+	 * <pre><code>
+	 *     String toPlainString() // 어떤 경우에도 다른 기호없이 숫자로만 표현
+	 *     String toString() // 필요하면 지수형태로 표현할 수도 있음
+	 * </code></pre>
+	 * <br>
+	 * 대부분의 경우 이 두 메서드의 반환결과가 같지만, BigDecimal을 생성할 때 '1.0e-22'아 같은 지수형태의 리터럴을 사용했을 때 다른 결과를 얻는 경우가 있다.<br>
+	 * BigDecimal도 Number로 부터 상속받은 기본형으로 변환하는 메서드들을 가지고 있다.<br>
+	 * <pre><code>
+	 *     int intValue()
+	 *     long longValue()
+	 *     float floatValue()
+	 *     double doubleValue()
+	 * </code></pre>
+	 * <br>
+	 * BigDecimal을 정수형으로 변환하는 메서드 중에서 이름 끝에 'Exact'가 붙은 것들은 변환한 결과가 반환한 타입의 범위에 속하지 않으면 ArithmeticException을 발생시킨다.<br>
+	 * <pre><code>
+	 *     byte byteValueExact()
+	 *     short shortValueExact()
+	 *     int intValueExact()
+	 *     long longValueExact()
+	 *     BigInteger toBigIntegerExact()
+	 * </code></pre>
+	 *
+	 */
+	class Memo14 {
+
+	}
+
+	/**
+	 * <h5>BigDecimal의 연산</h5><br>
+	 * <pre><code>
+	 *     BigDecimal add(BigDecimal val) // 덧셈(this + val)
+	 *     BigDecimal subtract(BigDecimal val) // 뺄셈(this - val)
+	 *     BigDecimal multiply(BigDecimal val) // 곱셈(this * val)
+	 *     BigDecimal divide(BigDecimal val) // 나눗셈(this / val)
+	 *     BigDecimal remainder(BigDecimal val) // 나머지(this % val)
+	 * </code></pre>
+	 * <br>
+	 * BigInteger와 마찬가지로 BigDecimal도 불변이므로, 반환타입이 BigDecimal인 경우 새로운 인스턴스가 반환된다.
+	 */
+	class Memo15 {
+
+	}
+
+	/**
+	 * <h5>반올림 모드 - divide()와 setScale()</h5><br>
+	 * 다른 연산과 달리 나눗셈을 처리하기 위한 메서드는 다음과 같이 다양한 버젼이 존재한다.<br>
+	 * 나숫셈의 결과를 어떻게 반올림(roundingMode)처리할 것인가와, 몇 번째 자리(scale)에서 반올림할 것인지를 지정할 수 있다.<br>
+	 * BigDecimal이 아무리 오차없이 실수를 저장한다해도 나눗셈에서 발생하는 오차는 어쩔 수 없다.<br>
+	 * <pre><code>
+	 *     BigDecimal divide(BigDecimal divisor)
+	 *     BigDecimal divide(BigDecimal divisor, int roundingMode)
+	 *     BigDecimal divide(BigDecimal divisor, RoundingMode roundingMode)
+	 *     BigDecimal divide(BigDecimal divisor, int scale, int roundingMode)
+	 *     BigDecimal divide(BigDecimal divisor, int scale, RoundingMode roundingMode)
+	 *     BigDecimal divide(BigDecimal divisor, MathContext mc)
+	 * </code></pre>
+	 * <br>
+	 * roundingMode는 반올림 처리방법에 대한 것으로 BigDecimal에 정의된 'ROUND_'로 시작하는 상수들 중에 하나를 선택해서 사용하면 된다.<br>
+	 * RoundingMode는 이 상수들을 열거형으로 정의한 것으로 나중에 추가되었다.<br>
+	 * 열거형 RoundingMode에 정의된 상수<br>
+	 * <table>
+	 *     <thead>
+	 *         <th>상수</th>
+	 *         <th>설명</th>
+	 *     </thead>
+	 *     <tbody>
+	 *         <tr>
+	 *             <td>CEILING</td>
+	 *             <td>올림</td>
+	 *         </tr>
+	 *         <tr>
+	 *             <td>FLOOR</td>
+	 *             <td>내림</td>
+	 *         </tr>
+	 *         <tr>
+	 *             <td>UP</td>
+	 *             <td>양수일 떄는 올림, 음수일 때는 내림</td>
+	 *         </tr>
+	 *         <tr>
+	 *             <td>DOWN</td>
+	 *             <td>양수일 때는 내림, 음수일 때는 올림(UP과 반대)</td>
+	 *         </tr>
+	 *         <tr>
+	 *             <td>HALF_UP</td>
+	 *             <td>반올림(5이상 올림, 5미만 버림)</td>
+	 *         </tr>
+	 *         <tr>
+	 *             <td>HALF_EVEN</td>
+	 *             <td>반올림(반올림 자리의 값이 짝수면 HALF_DOWN, 홀수면 HALF_UP)</td>
+	 *         </tr>
+	 *         <tr>
+	 *             <td>HALF_DOWN</td>
+	 *             <td>반올림(6이상 올림, 6미만 버림)</td>
+	 *         </tr>
+	 *         <tr>
+	 *             <td>UNNECESSARY</td>
+	 *             <td>나눗셈의 결과가 딱 떨어지는 수가 아니면, ArithmeticException발생</td>
+	 *         </tr>
+	 *     </tbody>
+	 * </table>
+	 * <br>
+	 * 주의해야할 점은 divide()로 나눈셈한 결과가 무한소수인 경우, 반올림 모드를 지정해주지 않으면 ArithmeticException이 발생한다는 것이다.
+	 */
+	class Memo16 {
+
+	}
+
+	/**
+	 * <h5>java.math.MathContext</h5><br>
+	 * 이 클래스는 반올림 모드와 정밀도(precision)을 하나로 묶어 놓은 것일 뿐 별다른 것은 없다.<br>
+	 * 한 가지 주의할 점은 divide()에서는 scale이 소수점 이하의 자리수를 의미하는데, MathContext에서는 precision이 정수와 소수점 이하를 모두 포함한 자리수를 의미한다는 것이다.
+	 */
+	class Memo17 {
+
+	}
+
+	/**
+	 * <h5>scale의 변경</h5><br>
+	 * BigDecimal을 10으로 곱하거나 나누는 대신 scale의 값을 변경함으로써 같은 결과를 얻을 수 있다. BigDecimal의 scale을 변경하려면, setScale()을 이용하면 된다.<br>
+	 * <pre><code>
+	 *     BigDecimal setScale(int newScale)
+	 *     BigDecimal setScale(int newScale, int roundingMode)
+	 *     BigDecimal setScale(int newScale, RoundingMode mode)
+	 * </code></pre>
+	 * <br>
+	 * setScale()로 scale을 값을 줄이는 것은 10의 n제곱으로 나누는 것과 같으므로, divide()를 호출할 때처럼 오차가 발생할 수 있고 반올리 모드를 지정해주어야 한다.
+	 */
+	class Memo18 {
 
 	}
 
